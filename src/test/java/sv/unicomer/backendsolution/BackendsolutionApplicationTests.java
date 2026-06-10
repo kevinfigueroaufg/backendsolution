a2c2316675b463315af76aa2f37aaafb844de4ea
kevinfigueroaufg <ia.kevinfigueroaufg@ufg.edu.sv>
2026-06-09 21:06:07 -0600

    Proyecto base primer commit

---
+++ src/test/java/sv/unicomer/backendsolution/BackendsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.unicomer.backendsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
